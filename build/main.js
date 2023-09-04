cd59106734bc777f39db6335440efecf4e3263fd
psk-98 <paulk198@hotmail.com>
2023-09-04 17:35:09 +0200

    add some minify for js files

---
+++ build/main.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".burger"),t=document.querySelector(".nav");e.addEventListener("click",(()=>{t.classList.toggle("open")}));
+//# sourceMappingURL=main.js.map
---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+const { src, dest, watch, series } = require("gulp")
+const terser = require("gulp-terser")
+const sourcemaps = require("gulp-sourcemaps")
+const concat = require("gulp-concat")
+
+function minifyJS() {
+  return src("dev/*.js")
+    .pipe(sourcemaps.init())
+    .pipe(terser({ toplevel: true }))
+    .pipe(concat("main.js")) // not really necessary as one js file
+    .pipe(sourcemaps.write("./"))
+    .pipe(dest("build/"))
+}
+
+function minifyCSS() {
+  gulp.task("critical", ["build"], function () {
+    critical.generate({
+      inline: true,
+      base: "./",
+      src: "index.html",
+      dest: "build/index.html",
+      minify: true,
+      width: 320,
+      height: 480,
+    })
+  })
+}
+
+function watchTask() {
+  watch("*.js", minifyJS)
+}
+
+exports.default = series(minifyJS, watchTask)
